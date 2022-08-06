417eade69efb3fa9bb1abe5ff05ca2940c10d9ae
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2022-08-07 00:14:05 +0300

    [REFC] Refactor list node

--- src/main/java/leetcode75/level1/day3/ListNode.java
+++ src/main/java/leetcode75/level1/day3/ListNode.java
@@ -2,17 +2,17 @@ package leetcode75.level1.day3;
 
 public class ListNode {
 
-  int val;
-  ListNode next;
+  public int val;
+  public ListNode next;
 
-  ListNode() {
+  public ListNode() {
   }
 
-  ListNode(int val) {
+  public ListNode(int val) {
     this.val = val;
   }
 
-  ListNode(int val, ListNode next) {
+  public ListNode(int val, ListNode next) {
     this.val = val;
     this.next = next;
   }
